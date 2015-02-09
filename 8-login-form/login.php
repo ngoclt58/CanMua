5ea0d936fdaeb9f267eb231af2b28e2abbc6c710
ngoclt_58 <ngoclt_58@vnu.edu.vn>
2015-02-09 13:11:38 +0700

    add sign in

---
+++ 8-login-form/login.php
@@ -0,0 +1,41 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "canmua";
+$conn = new mysqli($host, $user, $pass, $db);
+
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+$check = mysqli_query($conn,"SELECT * FROM account WHERE email='$email'");
+
+if(mysqli_num_row($check)>0)
+{
+   $mat_khau=mysqli($conn, "SELECT password FROM account WHERE email = '$email'");
+   if($password==$matkhau)
+   {
+       echo "Dang nhap thanh cong";
+   }
+   else
+   {
+       echo "Sai mat khau";
+   }
+}
+else
+{
+   echo "Tai khoan khong ton tai";
+}
+
+mysqli_close($conn);
+?>
+</body>
+</html>
---
+++ dangki/register.php
@@ -0,0 +1,29 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "canmua";
+$conn = new mysqli($host, $user, $pass, $db);
+if( !$conn){
+      //Không kết nối được, thoát ra và báo lỗi
+      die("không nết nối được vào MySQL server");
+};
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+mysqli_query($conn,"SELECT * FROM account");
+mysqli_query($conn,"INSERT INTO account (firstname,lastname,email,password) VALUES ('$firstname','$lastname','$email','$password')");
+mysqli_close($conn);
+?>
+</body>
+</html>
