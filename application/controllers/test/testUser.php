ee57e30d66a6135cab48b702245b2438bf32839c
trangnt58 <trang2uet@gmail.com>
2015-04-21 21:22:13 +0700

    Test phone of user

--- application/controllers/test/testUser.php
+++ application/controllers/test/testUser.php
@@ -1,19 +1,30 @@
 <?php
-require_once(APPPATH . '/controllers/home/user.php');
-class TestEmailandUser extends User
+//require_once(APPPATH . '/controllers/home/user.php');
+//test file User trong constroller/home
+require_once dirname(__FILE__).'/../home/user.php';
+
+
+class TestUser extends User
 {
+
 	public function __construct(){
         parent::__construct();
 		$this->load->library('unit_test');
     }
 	
+	function runTest()
+	{
+		$this->testCheckUser();
+		$this->testCheckEmail();
+		$this->testCheckPhone();
+		echo $this->unit->report();
+	}
 	function testCheckUser()
 	{	
 		$test=$this->checkUser("123");
 		$this->unit->run($test,TRUE,"test valid username");
 		
 		$this->unit->run($this->checkUser("hungnm"),FALSE, "test invalid username");
-		echo $this->unit->report();
 	}
 	
 	function testCheckEmail()
@@ -21,8 +32,13 @@ class TestEmailandUser extends User
 		$this->unit->run($this->checkEmail("hungnm_580@vnu.edu.vn"),FALSE,"Email has been exit");
 		$this->unit->run($this->checkEmail("a"),FALSE,"invalid email");
 		$this->unit->run($this->checkEmail("trangnt_580@vnu.edu.vn"),TRUE,"valid email");
-		echo $this->unit->report();
-		
 	}
+	
+	function testCheckPhone()
+	{
+		$this->unit->run($this->validPhone("01689399949"),TRUE,"valid Phone");
+		$this->unit->run($this->validPhone(1),FALSE,"invalid Phone");
+	}
+	
 }
 ?>
